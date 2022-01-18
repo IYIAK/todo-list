dd3325e5aa02739c130cbb9924c31be491189953
IYIAK <kaiyi0707@hotmail.com>
2022-01-18 14:27:35 +0800

    基本框架

---
+++ src/Box/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Search from '../Search'
+
+import './index.css'
+
+export default function Box() {
+    return (
+        <div className='box'>
+            <Search />
+        </div>
+    )
+}
---
+++ src/Search/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import './index.css'
+
+export default function Search() {
+    return (
+        <div id='searchBox'>
+            <input type="text" name="" class='searchText' />
+            <button class='search-btn'>Search</button>
+        </div>
+    )
+}
